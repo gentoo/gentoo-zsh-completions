e10c5b70a51f61f07c6aa416504b5c4aeba54f1d
Dawid Węgliński <cla@gentoo.org>
2008-01-13 15:26:31 +0100

    Improve the script

--- create-dist-bzip2.sh
+++ create-dist-bzip2.sh
@@ -5,4 +5,21 @@
 EXTRA_DIST="AUTHORS"
 COMPLETION_WIDGETS="_eselect _gcc-config _genlop _gentoolkit _gentoo_packages _layman _portage _portage_utils"
 
-${TAR} -jcf zsh-completion-$(date +%Y%m%d).tar.bz2 ${COMPLETION_WIDGETS} ${EXTRA_DIST}
+snapshot_date="$(date +%Y%m%d)"
+snapshot_dir="zsh-completion-${snapshot_date}"
+
+if [[ -d ${snapshot_dir} ]]; then
+	rm -rf ${snapshot_dir}
+	mkdir ${snapshot_dir}
+else
+	mkdir ${snapshot_dir}
+fi
+
+for i in ${EXTRA_DIST}; do
+	cp ${i} ${snapshot_dir}
+done
+for i in ${COMPLETION_WIDGETS}; do
+	cp ${i} ${snapshot_dir}
+done
+
+${TAR} -jcf ${snapshot_dir}.tar.bz2 ${snapshot_dir}
